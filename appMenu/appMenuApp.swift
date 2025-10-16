6258826d97dda28e94c6e6ddb83d16f9b3234171
Jeff Strupp <jmstrupp@tutanota.de>
2025-10-16 16:05:43 -0700

    Initial Commit

---
+++ appMenu/appMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  appMenuApp.swift
+//  appMenu
+//
+//  Created by Jeffrey Strupp on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct appMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
